ffc8510b1f058411d67657e7dd64a2d35e554a83
Dejan <dejotuturu@gmail.com>
2023-10-16 21:10:41 +0200

    db-con

--- includes/functions.php
+++
@@ -1,4 +0,0 @@
-
-function is_logged_in() {
-    return isset($_SESSION['user_id']);
-}
--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php
-// Include necessary files
-// require_once 'includes/db_connection.php';
-// require_once 'includes/session.php';
-// require_once 'includes/functions.php';
-
-// Check if the user is logged in
-// if (!is_logged_in()) {
-//     Redirect to the login page if not logged in
-//     header("Location: login.php");
-//     exit();
-// }
-
-// Retrieve user information (you may have a function for this in functions.php)
-// $user = get_user_by_id($_SESSION['user_id']);
-
-// Include the header template
 include 'templates/header.php';
 ?>
-
-<!-- Your HTML content for the homepage goes here -->
-
 <div>
     <h1>Welcome!</h1>
-    <!-- Display other content or links to various sections of your application -->
     <h2>User Registration</h2>
-    <form action="process_registration.php" method="POST">
+    <form action="./includes/process_registration.php" method="POST">
       <label for="username">Username:</label>
       <input type="text" id="username" name="username" required /><br />
 
@@ -37,7 +17,5 @@ include 'templates/header.php';
       <input type="submit" value="Register" />
     </form>
 </div>
-
-<!-- Include the footer template -->
 <?php include 'templates/footer.php'; ?>
 
