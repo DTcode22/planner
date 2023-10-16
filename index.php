87eb2f95b0debec5da2e579df68ad2383a0ce456
Dejan <dejotuturu@gmail.com>
2023-10-16 20:01:52 +0200

    user-registation

--- index.php
+++ index.php
@@ -23,6 +23,19 @@ include 'templates/header.php';
 <div>
     <h1>Welcome!</h1>
     <!-- Display other content or links to various sections of your application -->
+    <h2>User Registration</h2>
+    <form action="process_registration.php" method="POST">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required /><br />
+
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required /><br />
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required /><br />
+
+      <input type="submit" value="Register" />
+    </form>
 </div>
 
 <!-- Include the footer template -->
