bf9dc81833dc10759ee20ac84a5077bb5a35806b
Vuk Manojlovic <vukmanojlovic00@gmail.com>
2023-10-20 23:01:35 +0200

    Added basic login logic and reformated registration

---
+++ includes/database.php
@@ -0,0 +1,14 @@
+<?php
+require "database_info.php"
+
+function getDBConnection() {
+    global $db_host, $db_username, $db_password, $db_name;
+
+    $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+?>
